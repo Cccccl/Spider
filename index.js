5ce22b0159bfb34612f29f58f982260b02c2e297
Ccc1l <610178666@qq.com>
2018-01-03 00:00:13 +0800

    小说爬虫创建

---
+++ index.js
@@ -0,0 +1,37 @@
+const http = require ("http")
+const https = require('https');  
+const fs = require("fs")
+const cheerio = require('cheerio')
+const iconv = require("iconv-lite")
+const request = require("sync-request")
+const urlList = JSON.parse(fs.readFileSync('list.json','utf8'))
+
+let getContent = function (chapter,n){
+    https.get(chapter.link, function (res){
+        let chunks = []
+        res.on('data',function(chunk){
+            chunks.push(chunk)
+        })
+        res.on('end',function(){
+            let html = iconv.decode(Buffer.concat(chunks), 'utf8')
+            let $ = cheerio.load(html, {
+                decodeEntities: false
+            })
+            let content = $("div#content").text().replace(/(^\s*)/g, '\r\n#### ').replace(/\s{4}/g, '\r\n#### ');
+            if (fs.existsSync('./data/'+'全职高手 '+n+" "+chapter.title+'.md')){
+                fs.appendFileSync('./data/'+'全职高手 '+n+" "+chapter.title+'.md', '### ' + chapter.title)
+                fs.appendFileSync('./data/'+'全职高手 '+n+" "+chapter.title+'.md', content)
+            } else {
+                fs.writeFileSync('./data/' +'全职高手 '+n+" "+chapter.title+'.md', '### ' + chapter.title)
+                fs.appendFileSync('./data/' +'全职高手 '+n+" "+chapter.title+'.md', content)
+            }
+        })
+    }).on('error',function(){
+        console.log("爬取"+chapter.link+"链接出错！")
+    })
+
+}
+
+for(let i=0;i<urlList.length;i++){
+    getContent(urlList[i],i+1)
+}
---
+++ url.js
@@ -0,0 +1,42 @@
+var http = require("http")
+var https = require('https');  
+var fs = require("fs")
+var cheerio = require("cheerio")
+var iconv = require("iconv-lite")
+var url = 'https://www.xxbiquge.com/0_637/'
+
+https.get(url, function (res) {  //资源请求
+    var chunks = []
+    var length = 0;  
+    res.on('data', function (chunk) {
+        chunks.push(chunk)
+        length += chunk.length;  
+    })
+    res.on('end', function () {
+        var data = Buffer.concat(chunks, length);
+        // var change_data = iconv.decode(data, 'gb2312');
+        console.log(data.toString());  
+        // var html = iconv.decode(Buffer.concat(chunks), 'gb2312') //转码操作
+        var $ = cheerio.load(data, {
+            decodeEntities: false
+        })
+        var content = $("#list")
+        var links = []
+        $('dl').children('dd').each(function (i, elem) {
+            var link = new Object()
+            console.log($(this).children('a').text())
+            link.title = $(this).text()
+            link.link = 'https://www.xxbiquge.com' + $(this).children('a').attr('href') //补齐 URL 信息
+            if (i >= 0) {
+                links.push(link)
+            }
+        })
+        fs.writeFile("list.json", JSON.stringify(links), function (err) {
+            if (!err) {
+                console.log("写文件成功")
+            }
+        })
+    }).on('error', function () {
+        console.log("网页访问出错")
+    })
+})
